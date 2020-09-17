d213655e0eef29937641fc16c58ab5d11d3f32d0
trungnt1270 <71129889+trungnt1270@users.noreply.github.com>
2020-09-17 19:25:27 +0700

    ses8

---
+++ ses82.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main82()
+{
+	int num1,num2;
+	num1 = 540;
+	num2 = 243;
+	if (num1 > num2)
+		printf( "\n The greater number is: %d", num1);
+	else
+		printf("\n The greater number is: %d", num2);
+	return 0;
+}
